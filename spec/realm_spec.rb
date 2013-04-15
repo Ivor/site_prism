aa2c1bf7bb66a72872a549a8272d58fa9b75147c
Ivor Paul <ivorpaul@gmail.com>
2013-04-15 23:02:11 +0200

    Added spaces between examples.

--- spec/realm_spec.rb
+++ spec/realm_spec.rb
@@ -4,6 +4,7 @@ describe SitePrism::Realm do
   it "should respond to #page" do
     SitePrism::Realm.should respond_to(:page)
   end
+
   it "should respond to #realm" do
     SitePrism::Realm.should respond_to(:realm)
   end
@@ -17,6 +18,7 @@ describe SitePrism::Realm do
       my_realm = MyRealm.new
       my_realm.my_page.should be_a MyPage
     end
+
     it "should infer the page from the class name if provided" do
       class MyOtherPage < SitePrism::Page; end
       class MyRealm < SitePrism::Realm
@@ -26,6 +28,7 @@ describe SitePrism::Realm do
       my_realm.my_page.should be_a MyOtherPage
     end
   end
+
   describe "accessing a realm's realms" do
     it "should infer the name of the realm if no class name is given" do
       class MySubRealm < SitePrism::Realm; end
@@ -35,6 +38,7 @@ describe SitePrism::Realm do
       my_realm = MyRealm.new
       my_realm.my_sub_realm.should be_a MySubRealm
     end
+
     it "should infer the name of the realm if no class name is given" do
       class MyOtherRealm < SitePrism::Realm; end
       class MyRealm < SitePrism::Realm
@@ -44,6 +48,7 @@ describe SitePrism::Realm do
       my_realm.my_sub_realm.should be_a MyOtherRealm
     end
   end
+
   describe "navigating to pages through realms" do
     it "should allow access to pages in nested realms" do
       class UserIndexPage < SitePrism::Page; end
